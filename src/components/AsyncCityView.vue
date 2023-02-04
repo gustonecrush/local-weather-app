9f01796a40c0acb19b27afd67acc18e66af5f6b1
gustonecrush <codeboxezz@gmail.com>
2023-02-04 18:39:24 +0700

    displayed daily weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -78,6 +78,33 @@
         </div>
       </div>
     </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Weekly -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="m-4">7 Day Forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString('en-us', {
+                weekday: 'long',
+              })
+            }}
+          </p>
+          <img
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt="Weather Icon"
+            class="w-[50px] h-[50px] object-cover"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
