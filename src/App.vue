70389cd4ba4b2a576e08e1f1d6e80593e82d1719
gustonecrush <codeboxezz@gmail.com>
2023-02-04 12:20:00 +0700

    created base modal

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
-import SiteNavigation from './components/SiteNavigation.vue'
+  import { RouterView } from 'vue-router'
+  import SiteNavigation from './components/SiteNavigation.vue'
 </script>
 
-<style lang="scss" scoped></style>
