8a8aac8c17bd75cadfdc89c8557de24826b9300a
gustonecrush <codeboxezz@gmail.com>
2023-02-05 13:40:19 +0700

    route transitions

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 import CityView from "../views/CityView.vue";
 
 const router = createRouter({
@@ -9,13 +9,26 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: "/weather/:state/:city",
       name: "cityView",
       component: CityView,
+      meta: {
+        title: "Weather",
+      },
     },
   ],
 });
 
-export default router
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+  } | The Local Weather`;
+  next();
+});
+
+export default router;
